0c1e73c1aa8070a34bb5c6a910da24b04c3143cf
Ali Apple <toqeer.94@gmail.com>
2019-11-19 17:08:36 +0500

    Remove Task From LocalStorage

--- app.js
+++ app.js
@@ -142,7 +142,28 @@ function removeTask(e){
             e.target.parentElement.parentElement.remove();
         };
     }
+
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement);
 }
+// Remove Task from localstorage
+function removeTaskFromLocalStorage(taskItem){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.forEach(function(task, index){
+        if(taskItem.textContent === task){
+            tasks.splice(index, 1);
+        }
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+
 // Cler Tasks Funciton
 function clearTasks(){
     taskList.innerHTML = '';
